96fca67a5b602d7e8b43b7c5cd6efba9de5c1f17
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-09-30 18:03:28 -0400

    Cleaned up and refactored.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,25 +1,18 @@
-string = "z may a moody baby doom a yam"
-
 def to_caesar_ciph(string, offset)
   raise ArgumentError, 'Argument must be less than 26' unless offset < 26
-
-  key = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  split_string = string.downcase.split('')
-  cipher = ""
-
-  split_string.each do |e|
-    if e == " "
-      cipher.concat(" ")
+  key = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  string = string.downcase
+  cipher = ''
+  string.each_char do |e|
+    if e == ' '
+      cipher.concat(' ')
     else
       n = key.index(e) + offset
       if n >= 25
         n = key.index(e) + (offset - 26)
       end
-      # binding.pry
       cipher.concat(key[n])
     end
   end
   cipher
 end
-
-puts to_caesar_ciph(string, 26)

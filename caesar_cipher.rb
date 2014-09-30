986d56725031fc75899fcb5ba593b21b470086a5
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-09-30 12:40:25 -0400

    woring but further tests needed.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,18 +1,29 @@
 string = "may a moody baby doom a yam"
 
 def to_caesar_ciph(string, offset)
+  key = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   string = string.downcase
-  KEY = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  string = string.split('')
   cipher = ""
-  if offset > 26
-    string.each_char do |e|
-      n = KEY.index(e) + offset
-      cipher << KEY[n]
+
+  if offset <= 25
+    string.each do |e|
+      if e == " "
+        cipher << " "
+      else
+        n = key.index(e) + offset
+        if n > key.length
+          n = key.index(e) + (offset - key.length)
+        end
+        # binding.pry
+        cipher << key[n]
+      end
     end
+
+  else
+    return "please enter an offset of 25 or less"
   end
   cipher
 end
 
-to_caesar_ciph(string, 3)
-
-    # find index num of e in ALPH, add n to it. get string at that index and put it in cipher
+puts to_caesar_ciph(string, 3)

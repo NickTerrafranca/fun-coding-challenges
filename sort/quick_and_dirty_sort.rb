27f47833d034b052d9ec09f5662e0633277f8e4e
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-10-03 12:23:09 -0400

    Sorting with duplicate elements.

--- sort/quick_and_dirty_sort.rb
+++ sort/quick_and_dirty_sort.rb
@@ -2,20 +2,18 @@ require 'pry'
 
 def dirty_sort(list)
   sorted = []
-  l = list.length
   while list.length > 0
     n = list[0]
     list.each do |i|
-      if i <= n
+      if i < n
         n = i
       end
     end
-  sorted.push(n)
-  list.delete(n)
+  l = list.index(n)
+  sorted << list.slice!(l)
   end
-  # binding.pry
   sorted
 end
 
-list = [75, 100, -1, 85, 65, 84, 87, 95]
+list = [75, 100, -1, 85, 65, 84, 87, 65, 95]
 puts dirty_sort(list)

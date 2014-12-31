a1f2b229abfbe86d42d84bf940ed0648bd130ff8
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-12-31 09:55:53 -0500

    Expanded on the original code problem in order to account for an odd number of pages

---
+++ book_page_number/page_num_gen_two.rb
@@ -0,0 +1,24 @@
+NUMBER_OF_PAGES = 25
+def generate_pages(n)
+  first_page = 1
+  last_page = (n - 1)
+  remaining_page = n
+  page_sequence = []
+  if n % 2 == 1
+    (n/2).times do |i|
+      page_sequence[i] = [first_page, last_page]
+      first_page += 1
+      last_page -= 1
+    end
+    page_sequence << [nil, remaining_page]
+  else
+    last_page = n
+    (n/2).times do |i|
+      page_sequence[i] = [first_page, last_page]
+      first_page += 1
+      last_page -= 1
+    end
+  end
+  page_sequence
+end
+p generate_pages(NUMBER_OF_PAGES)

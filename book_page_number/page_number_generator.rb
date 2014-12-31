f39ccf2dfffd0adfcf28a089d61158e2063830c9
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-12-31 11:53:17 -0500

    cleanedup code

---
+++ book_page_number/page_number_generator.rb
@@ -0,0 +1,15 @@
+def generate_pages(n)
+  if n % 2 == 0 && n > 0
+    first_page = 1
+    last_page = n
+    page_sequence = []
+    (n/2).times do |i|
+      page_sequence[i] = [first_page, last_page]
+      first_page += 1
+      last_page -= 1
+    end
+  else
+    raise ArgumentError, 'Argument must be an even natural number'
+  end
+  page_sequence
+end

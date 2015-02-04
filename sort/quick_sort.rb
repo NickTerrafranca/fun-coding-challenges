506bc4d0f6760e8bfd1ccca813d0fe1bc6121a9b
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-02-04 15:01:47 -0500

    Have the partitioning process done

--- sort/quick_sort.rb
+++ sort/quick_sort.rb
@@ -1,5 +1,5 @@
 require 'pry'
-unsorted_array = [27, 77, 29, 43, 80, 96, 70, 94, 99, 82, 52, 41, 35, 57, 58, 86, 79, 90, 65, 46, 85, 97, 23, 59, 11, 15, 60, 88, 2, 63, 7, 54, 25, 47, 18, 26, 45, 36, 64, 56, 14, 69, 10, 37, 38, 3, 98, 20, 16, 5, 6, 21, 76, 81, 93, 0, 30, 33, 17, 61, 32, 87, 67, 55, 28, 92, 24, 31, 72, 34, 13, 62, 53, 8, 44, 48, 9, 12, 71, 75, 78, 66, 42, 4, 19, 68, 84, 1, 83, 74, 39, 40, 22, 49, 50, 73, 95, 51, 91, 89]
+unsorted_array = [77, 29, 43, 80, 96, 70, 27, 94, 99, 82]
 
 def quick_sort(list = nil, start_point = nil, end_point = nil)
 
@@ -7,37 +7,69 @@ def quick_sort(list = nil, start_point = nil, end_point = nil)
     left = start_point
     pivot = left
   else
-    left = list[0]
+    left = 0
     pivot = left
   end
 
   if end_point
     right = end_point
   else
-    right = list[-1]
+    right = list.index(list[-1])
   end
-
-  binding.pry
-
-  unless left == right
-    if pivot < right
-      right = right -=1
+  # binding.pry
+  while left < right
+    if list[pivot] < list[right]
+      right -= 1
     else
-      list[pivot] = right
-      list[right] = pivot
+      p = list[pivot]
+      r = list[right]
+      list[pivot] = r
+      list[right] = p
       pivot = right
-      if pivot > left
-        left = left += 1
-      else
-        list[pivot] = left
-        list[left] = pivot
-        pivot = left
-      end
+    end
+    if list[pivot] > list[left]
+      left += 1
+    else
+      p = list[pivot]
+      l = list[left]
+      list[pivot] = l
+      list[left] = p
+      pivot = left
     end
   end
-
   list
-  # binding.pry
+  binding.pry
 end
 
 p quick_sort(unsorted_array)
+
+
+# if list[pivot] > list[left]
+#   left += 1
+# else
+#   p = list[pivot]
+#   l = list[left]
+#   list[p] = list[l]
+#   list[l] = list[p]
+#   pivot = left
+# end
+
+
+# if list[pivot] > list[left]
+    #   left += 1
+    # else
+    #   p = list[pivot]
+    #   l = list[left]
+    #   list[p] = list[l]
+    #   list[l] = list[p]
+    #   pivot = left
+    #   if list[pivot] < list[right]
+    #     right -= 1
+    #   else
+    #     p = list[pivot]
+    #     r = list[right]
+    #     list[p] = list[r]
+    #     list[r] = list[p]
+    #     pivot = right
+    #   end
+    # end

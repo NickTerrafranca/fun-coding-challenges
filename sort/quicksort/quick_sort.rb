6d52aad7f15589660cf9dd188b276ddec531d0fc
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-02-07 16:29:00 -0500

    Added some comments to describe my thought process and logic

--- sort/quicksort/quick_sort.rb
+++ sort/quicksort/quick_sort.rb
@@ -1,9 +1,9 @@
 def quick_sort(list, start_point = nil, end_point = nil)
-  if list == []
+  if list == [] # Return an empty array if an empty array is passed
     return []
   end
 
-  if start_point && end_point
+  if start_point && end_point # This enables the initial call to quick_sort to require only the array as an argument
     if start_point >= end_point
       return list
     end
@@ -16,10 +16,10 @@ def quick_sort(list, start_point = nil, end_point = nil)
     pivot = 0
   end
 
-  min = left
+  min = left # These variables preserve the initial start and end values for later recursive calls
   max = right
 
-  while left < right
+  while left < right # The magic happens here
 
     if pivot == left
       if list[pivot] <= list[right]
@@ -34,11 +34,11 @@ def quick_sort(list, start_point = nil, end_point = nil)
       else
         list[pivot], list[left] = list[left], list[pivot]
         pivot = left
-     end
+      end
     else
       raise "Inconsistent state"
     end
   end
-  quick_sort(list, min, pivot -1)
-  quick_sort(list, pivot +1, max)
+  quick_sort(list, min, pivot -1) # Recursively sort the left portion of array
+  quick_sort(list, pivot +1, max) # Recursively sort the right portion of array
 end

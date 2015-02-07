c7d8f28eb302253a1a3e4b420132773c0bae4c4a
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-02-07 17:57:04 -0500

    Added more descriptive comments

--- sort/quicksort/quick_sort.rb
+++ sort/quicksort/quick_sort.rb
@@ -1,6 +1,6 @@
 def quick_sort(list, start_point = nil, end_point = nil)
   if list == [] # Return an empty array if an empty array is passed
-    return []
+    return list
   end
 
   if start_point && end_point # This enables the initial call to quick_sort to require only the array as an argument
@@ -19,18 +19,18 @@ def quick_sort(list, start_point = nil, end_point = nil)
   min = left # These variables preserve the initial start and end values for later recursive calls
   max = right
 
-  while left < right # The magic happens here
-    if pivot == left
-      if list[pivot] <= list[right]
+  while left < right # The magic starts here
+    if pivot == left # Start form the left
+      if list[pivot] <= list[right] # If the pivot is less than or equal to the right variable, move it left by one
         right -= 1
-      else
+      else # Otherwise if pivot is greater than right, swap pivot with right and point pivot to the right variable
         list[pivot], list[right] = list[right], list[pivot]
         pivot = right
       end
     elsif pivot == right
-      if list[pivot] >= list[left]
+      if list[pivot] >= list[left] # If the pivot is greater than or equal to the left variable, move right by one
         left += 1
-      else
+      else # Otherwise if pivot is less than left, swap pivot with left and point pivot to the left variable
         list[pivot], list[left] = list[left], list[pivot]
         pivot = left
       end

48fba1c03106e1134021435fa211fbbe8e3d13fb
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-03-07 17:35:02 -0500

    Working solution achieved   Please refactor and optimize

---
+++ sort/bubble_sort/bubble_sort.rb
@@ -0,0 +1,49 @@
+require 'pry'
+
+def bubble_sort(list)
+  if list == []
+    return list
+  end
+
+  end_of_list = list.length - 1
+  is_sorted = false
+  swpapped = false
+
+  left = 0
+  right = 1
+
+  while is_sorted == false
+
+    if swpapped == false && right == end_of_list
+      is_sorted = true
+
+    elsif list[left] > list[right]
+      list[left], list[right] = list[right], list[left]
+      swpapped = true
+      if right == end_of_list
+        left = 0
+        right = 1
+        swpapped = false
+      else
+        left +=1
+        right +=1
+      end
+
+    elsif right == end_of_list
+      left = 0
+      right = 1
+      swpapped = false
+
+    else
+      left +=1
+      right +=1
+    end
+
+  end
+  list
+end
+
+
+p bubble_sort([3, 7, 5, 6, 4, 0, 1, 2, 9, 8])
+# p bubble_sort([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# binding.pry

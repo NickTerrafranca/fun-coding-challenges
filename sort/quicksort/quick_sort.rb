fee258ce2ad0355f98b57094d59272735c812465
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-03-15 15:12:31 -0400

    Added a comment to better clarify my thought process

--- sort/quicksort/quick_sort.rb
+++ sort/quicksort/quick_sort.rb
@@ -21,21 +21,19 @@ def quick_sort(list, start_point = nil, end_point = nil)
 
   while left < right # The magic starts here
     if pivot == left # Start form the left
-      if list[pivot] <= list[right] # If the pivot is less than or equal to the right variable, move it left by one
+      if list[pivot] <= list[right] # If the pivot is less than or equal to the right variable, move right to the left by one
         right -= 1
       else # Otherwise if pivot is greater than right, swap pivot with right and point pivot to the right variable
         list[pivot], list[right] = list[right], list[pivot]
         pivot = right
       end
     elsif pivot == right
-      if list[pivot] >= list[left] # If the pivot is greater than or equal to the left variable, move right by one
+      if list[pivot] >= list[left] # If the pivot is greater than or equal to the left variable, move the left variable to the right by one
         left += 1
       else # Otherwise if pivot is less than left, swap pivot with left and point pivot to the left variable
         list[pivot], list[left] = list[left], list[pivot]
         pivot = left
       end
-    else
-      raise "Inconsistent state"
     end
   end
   quick_sort(list, min, pivot -1) # Recursively sort the left portion of array

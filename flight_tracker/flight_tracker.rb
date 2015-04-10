57a64aadebc6385f616d2fd3c9572e96c3475838
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-04-10 19:09:02 -0400

    Added comments to describe purpose of the code

--- flight_tracker/flight_tracker.rb
+++ flight_tracker/flight_tracker.rb
@@ -1,3 +1,6 @@
+# This sorts an array of airport codes in a way that would simulate a aircrafts flight path between multiple cities.
+# For example given an array [['LAX', 'BWI'], ['HNL', 'LAX'], ['SEA', 'HNL'], ['BOS', 'SEA']] where the first index of each sub array is the
+# departing airport and the second is the destination airport, the flight_path method would return [['BOS', 'SEA'], ['SEA', 'HNL'], ['HNL', 'LAX'], ['LAX', 'BWI']]
 class FlightTracker
 
   attr_reader :list, :start_point, :flight_path
